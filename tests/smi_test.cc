1d53bf4ae13e9d6bc33b1675f55ffcc72cff5794
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-07-15 22:08:01 +0200

    Adding some background regarding smi

--- tests/smi_test.cc
+++ tests/smi_test.cc
@@ -4,7 +4,22 @@
 
 /*
  * Small integer (SMI) example. These are tagged values.
- * more coming soon...
+ *
+ * Most computers architectures have a concept of pointer alignment meaning
+ * that a pointer to a particular data type must be a multiple of 8.
+ * So a pointer can be 8, 16, 24 etc
+ *
+ * 8  =  1000
+ * 16 = 10000
+ * 24 = 11000
+ *
+ * Remember that we are talking about the value stored in a memory location, 
+ * which in this case is interpreted as an address. It is this value that 
+ * will be a multiple of 8 like described above. As seen above there will be
+ * a three of the lowest order bits that will always be zero. This allows us to
+ * use them for other purposes. This is done as using a pointer for everything
+ * is not efficient. For example, using this for small integers 
+ *
  */
 TEST(Smi, IntToSmi) {
   v8::internal::Object* obj = v8::internal::IntToSmi<31>(2);
