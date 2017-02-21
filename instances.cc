f0f3b65fc220b39dfa3ffe328aa13382b18a991a
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-02-21 20:46:48 +0100

    use a Local<ObjectTemplate> as the global

--- instances.cc
+++ instances.cc
@@ -69,6 +69,8 @@ int main(int argc, char* argv[]) {
         Isolate::Scope isolate_scope(isolate);
         HandleScope handle_scope(isolate);
 
+        Local<ObjectTemplate> global = ObjectTemplate::New(isolate);
+
         Local<FunctionTemplate> function_template = FunctionTemplate::New(isolate, NewPerson);
         function_template->SetClassName(String::NewFromUtf8(isolate, "Person"));
         function_template->InstanceTemplate()->SetInternalFieldCount(1);
@@ -77,9 +79,10 @@ int main(int argc, char* argv[]) {
         Local<ObjectTemplate> person_template = ObjectTemplate::New(isolate, function_template);
         person_template->SetInternalFieldCount(1);
 
-        Local<Context> context = Context::New(isolate, NULL, person_template);
+        global->Set(String::NewFromUtf8(isolate, "Person", NewStringType::kNormal).ToLocalChecked(), function_template);
+
+        Local<Context> context = Context::New(isolate, NULL, global);
         Context::Scope context_scope(context);
-        auto ignored = context->Global()->Set(context, String::NewFromUtf8(isolate, "Person", NewStringType::kNormal).ToLocalChecked(), function_template->GetFunction());
 
         const char *js = "var user = new Person('Fletch'); user.name;";
         Local<String> source = String::NewFromUtf8(isolate, js, NewStringType::kNormal).ToLocalChecked();
