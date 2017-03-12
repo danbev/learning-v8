35ce9c7a7e24f2e468b378f31396ae14b8a1ca27
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-03-12 21:58:01 +0200

    add bta command to .lldbinit

---
+++ lldb_commands.py
@@ -0,0 +1,38 @@
+import lldb
+import re
+
+def bta(debugger, command, result, dict):
+  """Print stack trace with assertion scopes"""
+  func_name_re = re.compile("([^(<]+)(?:\(.+\))?")
+  assert_re = re.compile("^v8::internal::Per\w+AssertType::(\w+)_ASSERT, (false|true)>")
+  target = debugger.GetSelectedTarget()
+  process = target.GetProcess()
+  thread = process.GetSelectedThread()
+  frame = thread.GetSelectedFrame()
+  for frame in thread:
+    functionSignature = frame.GetDisplayFunctionName()
+    functionName = func_name_re.match(functionSignature)
+    line = frame.GetLineEntry().GetLine()
+    sourceFile = frame.GetLineEntry().GetFileSpec().GetFilename()
+    if line:
+      sourceFile = sourceFile + ":" + str(line)
+
+    if sourceFile is None:
+      sourceFile = ""
+    print("[%-2s] %-60s %-40s" % (frame.GetFrameID(), 
+                                  functionName.group(1),
+                                  sourceFile))
+    match = assert_re.match(functionSignature)
+    if match:
+      if match.group(3) == "false":
+        prefix = "Disallow"
+        color = "\033[91m"
+      else:
+        prefix = "Allow"
+        color = "\033[92m"
+      print("%s -> %s %s (%s)\033[0m" % (color, prefix, match.group(2), match.group(1)))
+    
+
+
+def __lldb_init_module (debugger, dict):
+  debugger.HandleCommand('command script add -f lldb-commands.bta bta')
