7ec65abae45c3a63a247e61b525275cb2fb37df0
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-02-03 14:13:38 +0100

    test: add context embedderdata test/example

--- tests/context_test.cc
+++ tests/context_test.cc
@@ -15,19 +15,33 @@ class ContextTest : public V8TestFixture {
 
 TEST_F(ContextTest, Scopes) {
   const Local<v8::ObjectTemplate> obt = v8::Local<v8::ObjectTemplate>();
-  const v8::HandleScope handle_scope(isolate_);
+  const v8::HandleScope handle_scope(V8TestFixture::isolate_);
   {
-    Handle<Context> context1 = Context::New(isolate_, nullptr, obt);
+    Handle<Context> context1 = Context::New(V8TestFixture::isolate_, nullptr, obt);
     Context::Scope contextScope1(context1);
     // entered_contexts_ [context1], saved_contexts_[isolateContext]
-    EXPECT_EQ(context1, isolate_->GetEnteredContext());
-    EXPECT_EQ(context1, isolate_->GetCurrentContext());
+    EXPECT_EQ(context1, V8TestFixture::isolate_->GetEnteredContext());
+    EXPECT_EQ(context1, V8TestFixture::isolate_->GetCurrentContext());
     {
-      Handle<Context> context2 = Context::New(isolate_, nullptr, obt);
+      Handle<Context> context2 = Context::New(V8TestFixture::isolate_, nullptr, obt);
       Context::Scope contextScope2(context2);
       // entered_contexts_ [context1, context2], saved_contexts[isolateContext, context1]
-      EXPECT_EQ(context2, isolate_->GetEnteredContext());
-      EXPECT_EQ(context2, isolate_->GetCurrentContext());
+      EXPECT_EQ(context2, V8TestFixture::isolate_->GetEnteredContext());
+      EXPECT_EQ(context2, V8TestFixture::isolate_->GetCurrentContext());
     }
   }
+}
+
+TEST_F(ContextTest, EmbedderData) {
+  const v8::HandleScope handle_scope(isolate_);
+  Handle<Context> context = Context::New(isolate_,
+                                         nullptr,
+                                         v8::Local<v8::ObjectTemplate>());
+  
+  Local<String> str = String::NewFromOneByte(isolate_, 
+      reinterpret_cast<const uint8_t*>("embdata"),
+      NewStringType::kNormal,
+      7).ToLocalChecked();
+  context->SetEmbedderData(0, str);
+  EXPECT_EQ(context->GetEmbedderData(0), str);
 }
