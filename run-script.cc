9775a8e597e5744a8e72733ce057a890275d73c3
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-02-26 06:00:42 +0100

    src: make run-script.cc compile
    Currently, this will compile but will segment fault when run which I'm
    going to look into but don't have enough time right now.

--- run-script.cc
+++ run-script.cc
@@ -4,48 +4,67 @@
 #include <string.h>
 #include <string>
 
+#include "src/objects/objects.h"
 #include "libplatform/libplatform.h"
 #include "v8.h"
 
 using namespace v8;
+
+extern void _v8_internal_Print_Object(void* object);
 
 v8::MaybeLocal<v8::String> ReadFile(v8::Isolate* isolate, const char* name);
 void Print(const v8::FunctionCallbackInfo<v8::Value>& args);
 
 class Person {
   private:
-    std::string name_;
+    char* name_;
 
   public:
-    Person(std::string name) : name_(name) {
+    Person(char* name) : name_(name) {
+      std::cout << "Person::Person name: " << name << '\n';
     }; 
 
-    std::string name() const { 
+    char* name() const { 
+      std::cout << "Person::Person name(): " << name_ << '\n';
       return name_;
     }
 
 };
 
 void NewPerson(const FunctionCallbackInfo<Value>& args) {
-    String::Utf8Value str(args[0]);
-    Person *p = new Person(*str);
-    Local<Object> self = args.Holder();
-    self->SetAlignedPointerInInternalField(0, p);
+    String::Utf8Value s(args.GetIsolate(), args[0]);
+    char* ss = *s;
+    std::cout << "NewPerson name: " << ss << '\n';
+    Person* p = new Person(ss);
+    HandleScope handle_scope(args.GetIsolate());
+    Local<Object> obj = args.Holder();
+    _v8_internal_Print_Object(* ((v8::internal::Object**) *obj));
+    obj->SetAlignedPointerInInternalField(0, p);
+    _v8_internal_Print_Object(* ((v8::internal::Object**) *obj));
 }
 
 void GetName(Local<String> property, const PropertyCallbackInfo<Value>& info) {
-  Local<Object> self = info.Holder();
-  Local<External> wrap = Local<External>::Cast(self->GetInternalField(0));
-  void* pointer = self->GetAlignedPointerFromInternalField(0);
-  const std::string value = static_cast<Person*>(pointer)->name();
-  info.GetReturnValue().Set(String::NewFromUtf8(info.GetIsolate(), value.c_str(), NewStringType::kNormal).ToLocalChecked());
+  Local<Object> obj = info.Holder();
+  printf("Get name field count.... %d\n", obj->InternalFieldCount());
+  _v8_internal_Print_Object(* ((v8::internal::Object**) *obj));
+  void* p = obj->GetAlignedPointerFromInternalField(0);
+
+  printf("Get name.... %p\n", p);
+  Person* person = static_cast<Person*>(p);
+  printf("Get name.... %p\n", person);
+  const std::string value = person->name();
+  printf("Get name.... %s\n", value.c_str());
+  info.GetReturnValue().Set(
+      String::NewFromUtf8(info.GetIsolate(), 
+                          value.c_str(),
+                          NewStringType::kNormal).ToLocalChecked());
 }
 
 int main(int argc, char* argv[]) {
   V8::InitializeExternalStartupData(argv[0]);
 
-  Platform* platform = platform::CreateDefaultPlatform();
-  V8::InitializePlatform(platform);
+  std::unique_ptr<Platform> platform = platform::NewDefaultPlatform();
+  V8::InitializePlatform(platform.get());
   V8::Initialize();
 
   Isolate::CreateParams create_params;
@@ -55,23 +74,37 @@ int main(int argc, char* argv[]) {
     Isolate::Scope isolate_scope(isolate);
     HandleScope handle_scope(isolate);
 
-    Local<ObjectTemplate> global = ObjectTemplate::New(isolate);
+    /*
+    ObjectTemplate* ot = *global;
+    v8::internal::Object** gpp = ((v8::internal::Object**)(ot));
+    v8::internal::Address* addr = (v8::internal::Address*) *gpp;
+    _v8_internal_Print_Object(addr);
+    _v8_internal_Print_Object(* ((v8::internal::Address**) *global));
+    _v8_internal_Print_Object(* ((v8::internal::Object**) *global));
+    */
 
     Local<FunctionTemplate> function_template = FunctionTemplate::New(isolate, NewPerson);
-    function_template->SetClassName(String::NewFromUtf8(isolate, "Person"));
+    function_template->SetClassName(String::NewFromUtf8(isolate, "Person").ToLocalChecked());
     function_template->InstanceTemplate()->SetInternalFieldCount(1);
-    function_template->InstanceTemplate()->SetAccessor(String::NewFromUtf8(isolate, "name"), GetName, nullptr);
+    function_template->InstanceTemplate()->SetAccessor(String::NewFromUtf8(isolate, "name").ToLocalChecked(), GetName, nullptr);
 
     Local<ObjectTemplate> person_template = ObjectTemplate::New(isolate, function_template);
     person_template->SetInternalFieldCount(1);
+    person_template->SetAccessor(String::NewFromUtf8(isolate, "name").ToLocalChecked(), GetName, nullptr);
 
-    global->Set(String::NewFromUtf8(isolate, "Person", NewStringType::kNormal).ToLocalChecked(), function_template);
-    global->Set(String::NewFromUtf8(isolate, "print", NewStringType::kNormal).ToLocalChecked(), FunctionTemplate::New(isolate, Print));
+    Local<ObjectTemplate> global = ObjectTemplate::New(isolate);
+    global->Set(String::NewFromUtf8(isolate, "Person", NewStringType::kNormal).ToLocalChecked(),
+        function_template);
+    global->Set(String::NewFromUtf8(isolate, "print", NewStringType::kNormal).ToLocalChecked(),
+        FunctionTemplate::New(isolate, Print));
 
     Local<Context> context = Context::New(isolate, NULL, global);
     Context::Scope context_scope(context);
 
-    Local<String> source = ReadFile(isolate, "script.js").ToLocalChecked();
+    //_v8_internal_Print_Object(((void*)(*global)));
+
+
+    Local<String> source = ReadFile(isolate, "/home/danielbevenius/work/google/learning-v8/script.js").ToLocalChecked();
     MaybeLocal<Script> script = Script::Compile(context, source).ToLocalChecked();
     MaybeLocal<Value> result = script.ToLocalChecked()->Run(context);
   }
@@ -80,7 +113,6 @@ int main(int argc, char* argv[]) {
   isolate->Dispose();
   V8::Dispose();
   V8::ShutdownPlatform();
-  delete platform;
   return 0;
 }
 
@@ -122,9 +154,8 @@ void Print(const v8::FunctionCallbackInfo<v8::Value>& args) {
     } else {
       printf(" ");
     }
-    v8::String::Utf8Value str(args[i]);
-    const char* cstr = *str;
-    printf("%s", cstr);
+    v8::String::Utf8Value str(args.GetIsolate(), args[i]);
+    printf("%s", *str);
   }
   printf("\n");
   fflush(stdout);
