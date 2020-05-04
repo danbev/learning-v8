1df0d2442fb6f41172706173cfc4987572f751d2
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-05-04 15:09:06 +0200

    src: add HoleyElement array test
    Docs and more examples will follow...

--- test/arrays_test.cc
+++ test/arrays_test.cc
@@ -5,6 +5,8 @@
 #include "v8_test_fixture.h"
 #include "src/objects/objects.h"
 #include "src/objects/objects-inl.h"
+#include "src/api/api.h"
+#include "src/objects/elements-kind.h"
 
 using namespace v8;
 namespace i = v8::internal;
@@ -51,4 +53,18 @@ TEST_F(ArraysTest, Array) {
     Local<Number> nr = Local<Number>::Cast(array->Get(context, i).ToLocalChecked());
     EXPECT_EQ(nr->Value(), i);
   }
+}
+
+TEST_F(ArraysTest, HoleyArray) {
+  Isolate::Scope isolate_scope(isolate_);
+  const v8::HandleScope handle_scope(isolate_);
+
+  Handle<v8::Context> context = Context::New(isolate_);
+  Context::Scope context_scope(context);
+
+  Local<Array> zero_length = Array::New(isolate_);
+  i::Handle<i::JSArray> js_array = Utils::OpenHandle(*zero_length);
+  print_handle(js_array);
+  EXPECT_TRUE(js_array->HasHoleyElements());
+  EXPECT_EQ(js_array->GetElementsKind(), i::ElementsKind::HOLEY_ELEMENTS);
 }
