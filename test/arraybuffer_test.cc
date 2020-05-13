b96b63960bafe49bc794ab421e0fc5b685c9ca94
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-05-13 15:04:23 +0200

    test: add ArrayBuffer example

---
+++ test/arraybuffer_test.cc
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h"
+#include "v8.h"
+#include "v8_test_fixture.h"
+
+using namespace v8;
+
+class ArrayBufferTest : public V8TestFixture {
+};
+
+TEST_F(ArrayBufferTest, ArrayBuffer) {
+  Isolate::Scope isolate_scope(isolate_);
+  const v8::HandleScope handle_scope(isolate_);
+
+  Handle<v8::Context> context = Context::New(isolate_);
+  Context::Scope context_scope(context);
+
+  Local<ArrayBuffer> ab = ArrayBuffer::New(isolate_, 8);
+  EXPECT_EQ(static_cast<int>(ab->ByteLength()), 8);
+
+  // A BackingStore is a wrapper around raw memory
+  std::shared_ptr<BackingStore> backingstore = ab->GetBackingStore();
+  EXPECT_EQ(static_cast<int>(backingstore->ByteLength()), 8);
+}
+
+TEST_F(ArrayBufferTest, Uint16Array) {
+  Isolate::Scope isolate_scope(isolate_);
+  const v8::HandleScope handle_scope(isolate_);
+
+  Handle<v8::Context> context = Context::New(isolate_);
+  Context::Scope context_scope(context);
+
+  Local<ArrayBuffer> ab = ArrayBuffer::New(isolate_, 32);
+  Local<Uint16Array> array = Uint16Array::New(ab, 0, 2);
+  EXPECT_EQ(static_cast<int>(array->Length()), 2);
+  array->Set(context, 0, Number::New(isolate_, 220)).Check();
+  array->Set(context, 1, Number::New(isolate_, 221)).Check();
+  print_local(array);
+}
