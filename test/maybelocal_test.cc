d14caa4c58d12f9712febce0244a40dc591bca8f
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-05-22 14:02:51 +0200

    src: add MaybeLocal::FromMaybe example

--- test/maybelocal_test.cc
+++ test/maybelocal_test.cc
@@ -8,7 +8,7 @@ using namespace v8;
 class MaybeLocalTest : public V8TestFixture {
 };
 
-TEST_F(MaybeLocalTest, MaybeLocal) {
+TEST_F(MaybeLocalTest, Basic) {
   Isolate::Scope isolate_scope(isolate_);
   const HandleScope handle_scope(isolate_);
   Handle<Context> context = Context::New(isolate_);
@@ -17,6 +17,20 @@ TEST_F(MaybeLocalTest, MaybeLocal) {
   MaybeLocal<Value> m;
   EXPECT_TRUE(m.IsEmpty());
   ASSERT_DEATH(m.ToLocalChecked(), "Fatal error");
+
+  // the {} will use the types, MaybeLocal default constructor so this would
+  // be the same as writing MaybeLocal<Value> something = MaybeLocal<Value>();
+  MaybeLocal<Value> something = {};
+  EXPECT_TRUE(something.IsEmpty());
+  MaybeLocal<Value> something2 = MaybeLocal<Value>();
+  EXPECT_TRUE(something2.IsEmpty());
+}
+
+TEST_F(MaybeLocalTest, ToLocal) {
+  Isolate::Scope isolate_scope(isolate_);
+  const HandleScope handle_scope(isolate_);
+  Handle<Context> context = Context::New(isolate_);
+  Context::Scope context_scope(context);
 
   Local<Number> nr = Number::New(isolate_, 18);
   MaybeLocal<Number> maybe_nr = MaybeLocal<Number>(nr);
@@ -28,11 +42,22 @@ TEST_F(MaybeLocalTest, MaybeLocal) {
   EXPECT_TRUE(maybe_nr.ToLocal<Number>(&nr2));
   EXPECT_TRUE(maybe_nr.ToLocal(&nr2));
   EXPECT_EQ(nr2->Value(), 18);
+}
 
-  // the {} will use the types, MaybeLocal default constructor so this would
-  // be the same as writing MaybeLocal<Value> something = MaybeLocal<Value>();
-  MaybeLocal<Value> something = {};
-  std::cout << something.IsEmpty() << '\n';
-  MaybeLocal<Value> something2 = MaybeLocal<Value>();
-  std::cout << something2.IsEmpty() << '\n';
+TEST_F(MaybeLocalTest, FromMaybe) {
+  Isolate::Scope isolate_scope(isolate_);
+  const HandleScope handle_scope(isolate_);
+  Handle<Context> context = Context::New(isolate_);
+  Context::Scope context_scope(context);
+
+  Local<String> str = String::NewFromUtf8Literal(isolate_, "bajja");
+  MaybeLocal<String> maybe_str = MaybeLocal<String>(str);
+  Local<Value> from_local = maybe_str.FromMaybe<Value>(Local<Value>());
+  EXPECT_FALSE(from_local.IsEmpty());
+  String::Utf8Value value(isolate_, from_local);
+  EXPECT_STREQ("bajja", *value);
+
+  maybe_str = MaybeLocal<String>();
+  from_local = maybe_str.FromMaybe<Value>(Local<Value>());
+  EXPECT_TRUE(from_local.IsEmpty());
 }
