52991b46e7c8de8f683faf6d90ced54454d2e40c
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-05-26 11:47:25 +0200

    src: add Maybe<void> example

--- test/maybe_test.cc
+++ test/maybe_test.cc
@@ -23,3 +23,29 @@ TEST_F(MaybeTest, Maybe) {
   int value = nothing.FromMaybe(22);
   EXPECT_EQ(value, 22);
 }
+
+/*
+ * I think the intention with a type Maybe<void> is that we don't really
+ * care/want to have a value in the Maybe apart from that is is empty or
+ * something. So instead of having a bool and setting it to true just
+ * have void and return an empty. I think this signals the intent of a
+ * function better as one might otherwise wonder what the value in the maybe
+ * represents.
+ */
+Maybe<void> doit(int x) {
+  if (x == -1) {
+    return Nothing<void>();
+  }
+  return JustVoid();
+}
+
+TEST_F(MaybeTest, MaybeVoid) {
+  Maybe<void> maybe = JustVoid();
+  EXPECT_FALSE(maybe.IsNothing());
+
+  Maybe<void> maybe_nothing = Nothing<void>();
+  EXPECT_TRUE(maybe_nothing.IsNothing());
+
+  EXPECT_TRUE(doit(-1).IsNothing());
+  EXPECT_TRUE(doit(1).IsJust());
+}
