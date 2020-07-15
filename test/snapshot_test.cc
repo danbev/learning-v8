42cd997d09f0c8fb3ee5dd5b06879bfc27ce560d
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-07-15 15:23:01 +0200

    src: remove exeption handling and add comments

--- test/snapshot_test.cc
+++ test/snapshot_test.cc
@@ -8,12 +8,12 @@ using namespace v8;
 extern void _v8_internal_Print_Object(void* object);
 
 TEST(SnapshotTest, CreateSnapshot) {
-  std::cout << "SnapshotTest::CreateSnapshot\n";
   v8::V8::SetFlagsFromString("--random_seed=42");
   std::unique_ptr<Platform> platform = platform::NewDefaultPlatform();
   V8::InitializePlatform(platform.get());
   V8::Initialize();
   StartupData startup_data;
+  size_t index;
 
   std::vector<intptr_t> external_references = { reinterpret_cast<intptr_t>(nullptr)};
   Isolate* isolate = Isolate::Allocate();
@@ -26,24 +26,21 @@ TEST(SnapshotTest, CreateSnapshot) {
       {
         Context::Scope context_scope(context);
         TryCatch try_catch(isolate);
+
+        // Add the following function to the context
         const char* js = R"(function test_snapshot() { 
-          return 'from test_snaphot function';
-        }
-        test_snapshot();)";
+          return 'from test_snapshot function';
+        })";
         Local<v8::String> src = String::NewFromUtf8(isolate, js).ToLocalChecked();
         ScriptOrigin origin(String::NewFromUtf8Literal(isolate, "test_snapshot"));
         ScriptCompiler::Source source(src, origin);                     
         Local<v8::Script> script;
         EXPECT_TRUE(ScriptCompiler::Compile(context, &source).ToLocal(&script));
-        Local<Value> result = script->Run(context).ToLocalChecked();
-        //String::Utf8Value utf8(isolate, result);
-        //std::cout << "result from executing script: " << *utf8 << '\n';
-
+        EXPECT_FALSE(script->Run(context).ToLocalChecked().IsEmpty());
         EXPECT_FALSE(try_catch.HasCaught());
-        //snapshot_creator.SetDefaultContext(context);
       }
 
-      size_t index = snapshot_creator.AddContext(context);
+      index = snapshot_creator.AddContext(context);
       std::cout << "context index: " << index << '\n';
     }
     startup_data = snapshot_creator.CreateBlob(SnapshotCreator::FunctionCodeHandling::kKeep);
@@ -62,36 +59,30 @@ TEST(SnapshotTest, CreateSnapshot) {
   V8::InitializePlatform(platform.get());
   V8::Initialize();
   Isolate::CreateParams create_params;
+  // Specify the startup_data blob created earlier.
   create_params.snapshot_blob = &startup_data;
   create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
   isolate = Isolate::New(create_params);
   {
     HandleScope scope(isolate);
-    Local<Context> context = Context::FromSnapshot(isolate, 0).ToLocalChecked();
+    // Create the Context from the snapshot index.
+    Local<Context> context = Context::FromSnapshot(isolate, index).ToLocalChecked();
     Context::Scope context_scope(context);
     TryCatch try_catch(isolate);
+    // Add JavaScript code that calls the function we added previously.
     const char* js = "test_snapshot();";
     Local<v8::String> src = String::NewFromUtf8(isolate, js).ToLocalChecked();
     ScriptOrigin origin(String::NewFromUtf8Literal(isolate, "test_snapshot"));
     ScriptCompiler::Source source(src, origin);                     
     Local<v8::Script> script;
     EXPECT_TRUE(ScriptCompiler::Compile(context, &source).ToLocal(&script));
-    MaybeLocal<Value> res = script->Run(context);
-    if (res.IsEmpty()) {
-      std::cout << std::boolalpha << "was an exception thrown: " << try_catch.HasCaught() << '\n';
-      Local<Object> obj = try_catch.Exception()->ToObject(context).ToLocalChecked();
-      Local<v8::String> msg_key = String::NewFromUtf8(isolate, "message").ToLocalChecked();
-      Local<Value> message = obj->Get(context, msg_key).ToLocalChecked();
-      Local<String> str = message->ToString(context).ToLocalChecked();
-      String::Utf8Value e(isolate, str);
-      std::cout << "exception: " << *e << '\n';
-    } else {
-      Local<Value> result = res.ToLocalChecked();
-      String::Utf8Value utf8(isolate, result);
-      std::cout << "result from executing script: " << *utf8 << '\n';
+    MaybeLocal<Value> maybe_result = script->Run(context);
+    EXPECT_FALSE(maybe_result.IsEmpty());
 
-      EXPECT_FALSE(try_catch.HasCaught());
-    }
+    Local<Value> result = maybe_result.ToLocalChecked();
+    String::Utf8Value utf8(isolate, result);
+    EXPECT_STREQ("from test_snapshot function", *utf8);
+    EXPECT_FALSE(try_catch.HasCaught());
   }
   v8::V8::ShutdownPlatform();
 }
