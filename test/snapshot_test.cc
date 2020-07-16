b6ccb184ae0b7b2de58ca0a9c428199d44c9d817
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-07-16 12:46:36 +0200

    src: add AddData example

--- test/snapshot_test.cc
+++ test/snapshot_test.cc
@@ -13,17 +13,30 @@ void print_data(StartupData* startup_data) {
   }
 }
 
-TEST(SnapshotTest, CreateSnapshot) {
-  v8::V8::SetFlagsFromString("--random_seed=42");
-  std::unique_ptr<Platform> platform = platform::NewDefaultPlatform();
-  V8::InitializePlatform(platform.get());
-  V8::Initialize();
+class SnapshotTest : public ::testing::Test {
+ public:
+ protected:
+  static std::unique_ptr<v8::Platform> platform_;
+  static void SetUpTestCase() {
+    platform_ = v8::platform::NewDefaultPlatform();
+    v8::V8::SetFlagsFromString("--random_seed=42");
+    v8::V8::InitializePlatform(platform_.get());
+    v8::V8::Initialize();
+  }
+  static void TearDownTestCase() {
+    v8::V8::ShutdownPlatform();
+  }
+};
+
+std::unique_ptr<v8::Platform> SnapshotTest::platform_;
+
+TEST_F(SnapshotTest, CreateSnapshot) {
   StartupData startup_data;
   size_t index;
 
   std::vector<intptr_t> external_references = { reinterpret_cast<intptr_t>(nullptr)};
-  Isolate* isolate = Isolate::Allocate();
   {
+    Isolate* isolate = Isolate::Allocate();
     v8::SnapshotCreator snapshot_creator(isolate, external_references.data());
     {
       HandleScope scope(isolate);
@@ -52,15 +65,13 @@ TEST(SnapshotTest, CreateSnapshot) {
     startup_data = snapshot_creator.CreateBlob(SnapshotCreator::FunctionCodeHandling::kKeep);
     std::cout << "size of blob: " << startup_data.raw_size << '\n';
   }
-  v8::V8::ShutdownPlatform();
+  // SnapshotCreators destructor will call Isolate::Exit and Isolate::Dispose
 
-  V8::InitializePlatform(platform.get());
-  V8::Initialize();
   Isolate::CreateParams create_params;
   // Specify the startup_data blob created earlier.
   create_params.snapshot_blob = &startup_data;
   create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-  isolate = Isolate::New(create_params);
+  Isolate* isolate = Isolate::New(create_params);
   {
     HandleScope scope(isolate);
     // Create the Context from the snapshot index.
@@ -82,5 +93,69 @@ TEST(SnapshotTest, CreateSnapshot) {
     EXPECT_STREQ("from test_snapshot function", *utf8);
     EXPECT_FALSE(try_catch.HasCaught());
   }
-  v8::V8::ShutdownPlatform();
+  isolate->Dispose();
 }
+
+
+void InternalFieldsCallback(Local<Object> holder, int index,
+                            StartupData payload, void* data) {
+  std::cout << "InternalFieldsCallback..." << '\n';
+}
+
+class ExternalData {
+ public:
+  ExternalData(int x) : x_(x) {}
+  int x() { return x_; }
+ private:
+  int x_;
+};
+
+TEST_F(SnapshotTest, CreateSnapshotWithData) {
+  StartupData startup_data;
+  size_t index;
+  size_t data_index;
+
+  std::vector<intptr_t> external_references = { reinterpret_cast<intptr_t>(nullptr)};
+  {
+    Isolate* isolate = nullptr;
+    isolate = Isolate::Allocate();
+    v8::SnapshotCreator snapshot_creator(isolate, external_references.data());
+    {
+      HandleScope scope(isolate);
+      snapshot_creator.SetDefaultContext(Context::New(isolate));
+      Local<Context> context = Context::New(isolate);
+      Local<Number> data = Number::New(isolate, 18);
+      data_index = snapshot_creator.AddData(context, data);
+      std::cout << "data_index: " << data_index << '\n';
+      index = snapshot_creator.AddContext(context);
+      std::cout << "context index: " << index << '\n';
+    }
+    startup_data = snapshot_creator.CreateBlob(SnapshotCreator::FunctionCodeHandling::kKeep);
+    std::cout << "size of blob: " << startup_data.raw_size << '\n';
+  }
+
+  Isolate::CreateParams create_params;
+  // Specify the startup_data blob created earlier.
+  create_params.snapshot_blob = &startup_data;
+  create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+  Isolate* isolate = Isolate::New(create_params);
+  {
+    HandleScope scope(isolate);
+    const void* data = "some data";
+    DeserializeInternalFieldsCallback int_fields_cb(InternalFieldsCallback,
+                                                    const_cast<void*>(data));
+    // Create the Context from the snapshot index.
+    Local<Context> context = Context::FromSnapshot(isolate,
+      index,
+      int_fields_cb).ToLocalChecked();
+    Context::Scope context_scope(context);
+    MaybeLocal<Object> obj = context->GetDataFromSnapshotOnce<Object>(data_index);
+    EXPECT_FALSE(obj.IsEmpty());
+    Local<Object> o = obj.ToLocalChecked();
+    Local<Number> nr = o.As<Number>();
+    std::cout << "data: " << nr->Value() << '\n';
+    EXPECT_EQ(18, nr->Value());
+  }
+  isolate->Dispose();
+}
+
