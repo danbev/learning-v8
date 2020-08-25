054510ec8af47c692d32a033f8cc19c4791bf161
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-08-25 09:07:52 +0200

    src: make ast_test compile again
    There is still a linking error that I'm looking into.

--- test/ast_test.cc
+++ test/ast_test.cc
@@ -7,7 +7,7 @@
 #include "src/parsing/parser.h"
 #include "src/parsing/parsing.h"
 #include "src/parsing/scanner-character-streams.h"
-#include "src/globals.h"
+#include "src/common/globals.h"
 
 using namespace v8;
 namespace i = v8::internal;
@@ -23,14 +23,18 @@ TEST_F(AstTest, ParseInfo) {
   i::Handle<i::String> source = factory->NewStringFromStaticChars(
       "const msg = 'testing'");
   i::Handle<i::Script> script = factory->NewScript(source);
-  i::ParseInfo parse_info(script);
-  EXPECT_TRUE(parse_info.is_toplevel());
-  EXPECT_FALSE(parse_info.is_eval());
-  EXPECT_FALSE(parse_info.is_strict_mode());
-  EXPECT_FALSE(parse_info.is_native());
-  EXPECT_FALSE(parse_info.is_module());
-  EXPECT_EQ(parse_info.start_position(), 0) << "start line (I think) was not 0";
-  EXPECT_EQ(parse_info.end_position(), 0) << "end line (I think) was not 0";
+  i::UnoptimizedCompileState state(internal_isolate);
+  i::UnoptimizedCompileFlags compile_flags = i::UnoptimizedCompileFlags::ForScriptCompile(
+      internal_isolate, *script);
+  i::ParseInfo parse_info(internal_isolate, compile_flags, &state);
+  EXPECT_TRUE(compile_flags.is_toplevel());
+  EXPECT_FALSE(compile_flags.is_eval());
+  //EXPECT_FALSE(parse_info.is_strict_mode());
+  //EXPECT_FALSE(compile_flags.is_native());
+  EXPECT_FALSE(compile_flags.is_module());
+  i::Scope* scope = parse_info.literal()->scope();
+  EXPECT_EQ(scope->start_position(), 0) << "start line (I think) was not 0";
+  EXPECT_EQ(scope->end_position(), 0) << "end line (I think) was not 0";
 }
 
 TEST_F(AstTest, Parser) {
@@ -41,12 +45,15 @@ TEST_F(AstTest, Parser) {
   i::Handle<i::String> source = factory->NewStringFromStaticChars(
       "const msg = 'testing'");
   i::Handle<i::Script> script = factory->NewScript(source);
-  i::ParseInfo parse_info(script);
-  bool result = i::parsing::ParseProgram(&parse_info, internal_isolate);
+  i::UnoptimizedCompileState state(internal_isolate);
+  i::UnoptimizedCompileFlags compile_flags = i::UnoptimizedCompileFlags::ForScriptCompile(
+      internal_isolate, *script);
+  i::ParseInfo parse_info(internal_isolate, compile_flags, &state);
+  bool result = i::parsing::ParseProgram(&parse_info,
+      script, internal_isolate, i::parsing::ReportStatisticsMode::kYes);
   EXPECT_TRUE(result);
   // ParseProgram set parse_info's literal_ which is a AST node
   i::FunctionLiteral* function_literal = parse_info.literal();
-  EXPECT_EQ(function_literal->function_type(), i::FunctionLiteral::FunctionType::kAnonymousExpression);
+  EXPECT_EQ(function_literal->syntax_kind(), i::FunctionSyntaxKind::kAnonymousExpression);
   EXPECT_EQ(function_literal->kind(), i::FunctionKind::kNormalFunction);
 }
-
