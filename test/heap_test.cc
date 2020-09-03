b368969c0cbe06ea53fccc2c45ab41278ca07a75
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-09-03 15:14:19 +0200

    src: add more heap exploration code

--- test/heap_test.cc
+++ test/heap_test.cc
@@ -26,6 +26,7 @@ TEST_F(HeapTest, AllocateRaw) {
   i::Isolate* internal_isolate = asInternal(isolate_);
 
   v8::internal::Heap* heap = internal_isolate->heap();
+  //i::CodeLargeObjectSpace* lo_space = new i::CodeLargeObjectSpace(heap);
   i::CodeLargeObjectSpace lo_space(heap);
   i::AllocationResult result = lo_space.AllocateRaw(64);
   EXPECT_EQ(result.IsRetry(), false);
@@ -53,9 +54,52 @@ TEST_F(HeapTest, RegisterNewlyAllocatedCodeObject) {
   v8::internal::Heap* heap = internal_isolate->heap();
   i::CodeLargeObjectSpace lo_space(heap);
   i::AllocationResult result = lo_space.AllocateRaw(64);
-  i::HeapObject object = result.ToObject();
-  i::MemoryChunk* chunk = i::MemoryChunk::FromHeapObject(object);
+  i::HeapObject heap_object;
+  ASSERT_TRUE(result.To(&heap_object));
+  i::Address base_address = i::BasicMemoryChunk::BaseAddress(heap_object.ptr());
+  i::BasicMemoryChunk* b = reinterpret_cast<i::BasicMemoryChunk*>(base_address);
+  i::MemoryChunk* c = static_cast<i::MemoryChunk*>(b);
+  ASSERT_TRUE(c->GetCodeObjectRegistry() != nullptr);
+
+  i::BasicMemoryChunk* basic = i::BasicMemoryChunk::FromHeapObject(heap_object);
+  i::MemoryChunk* chunk = i::MemoryChunk::FromHeapObject(heap_object);
   i::CodeObjectRegistry* r = chunk->GetCodeObjectRegistry();
 
-  ASSERT_DEATH(r->RegisterNewlyAllocatedCodeObject(object.address()), "");
+  ASSERT_DEATH(r->RegisterNewlyAllocatedCodeObject(heap_object.address()), "");
+}
+
+TEST_F(HeapTest, BasicMemoryChunk) {
+  const v8::HandleScope handle_scope(isolate_);
+  Isolate::Scope isolate_scope(isolate_);
+  i::Isolate* internal_isolate = asInternal(isolate_);
+  i::Address base = i::kNullAddress;
+  i::Address start = i::kNullAddress;
+  i::Address end = i::kNullAddress;
+  v8::internal::Heap* heap = internal_isolate->heap();
+  i::CodeLargeObjectSpace lo_space(heap);
+  i::FreeList* freelist = i::FreeList::CreateFreeList();
+  /*
+  i::PagedSpace owner_space(heap,
+      i::AllocationSpace::NEW_SPACE, 
+      i::Executability::NOT_EXECUTABLE,
+      freelist);
+  */
+  i::BasicMemoryChunk* chunk = i::BasicMemoryChunk::Initialize(heap,
+      base, 32, start, end, &lo_space, i::VirtualMemory()); 
+}
+
+TEST_F(HeapTest, Space) {
+  const v8::HandleScope handle_scope(isolate_);
+  Isolate::Scope isolate_scope(isolate_);
+  i::Isolate* internal_isolate = asInternal(isolate_);
+  i::Address base = i::kNullAddress;
+  i::Address start = i::kNullAddress;
+  i::Address end = i::kNullAddress;
+  v8::internal::Heap* heap = internal_isolate->heap();
+  i::FreeList* freelist = i::FreeList::CreateFreeList();
+  i::PagedSpace space(heap,
+      i::AllocationSpace::NEW_SPACE, 
+      i::Executability::NOT_EXECUTABLE,
+      freelist);
+
 }
