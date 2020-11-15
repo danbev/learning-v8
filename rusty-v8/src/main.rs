8966bb4bb2a87f026b08dfc69f0c60e0c912c576
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-11-15 17:23:08 +0100

    src: add doit function to deno example

--- rusty-v8/src/main.rs
+++ rusty-v8/src/main.rs
@@ -1,5 +1,14 @@
 use rusty_v8 as v8;
 
+fn doit(
+  scope: &mut v8::HandleScope,
+  args: v8::FunctionCallbackArguments,
+  mut _retval: v8::ReturnValue,
+) {
+  let str = args.get(0).to_string(scope).unwrap().to_rust_string_lossy(scope);
+  println!("doit argument = {}", str);
+}
+
 fn main() {
     println!("Example of using rusty-v8: {}", v8::V8::get_version());
 
@@ -8,7 +17,24 @@ fn main() {
     v8::V8::initialize();
     
     // TODO: implement the same functionality as ../hello-world.cc
+    let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
+    let handle_scope = &mut v8::HandleScope::new(isolate);
+
+    let global = v8::ObjectTemplate::new(handle_scope);
+
+    global.set(
+      v8::String::new(handle_scope, "doit").unwrap().into(),
+      v8::FunctionTemplate::new(handle_scope, doit).into(),
+    );
+
+    let context = v8::Context::new_from_template(handle_scope, global);
+    let context_scope = &mut v8::ContextScope::new(handle_scope, context);
+
+    let source = v8::String::new(context_scope, "const age = 40; doit(age);").unwrap();
+    let script = v8::Script::compile(context_scope, source, None).unwrap();
+    let result = script.run(context_scope).unwrap();
+    let result = result.to_string(context_scope).unwrap();
+    println!("{}", result.to_rust_string_lossy(context_scope));
 
     unsafe { v8::V8::dispose(); }
-    v8::V8::shutdown_platform();
 }
